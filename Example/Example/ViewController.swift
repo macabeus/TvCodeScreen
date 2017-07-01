0b1c54162cfe819763e6924b276a3a863ef74c41
Bruno Macabeus Aquino <bruno.macabeus@gmail.com>
2017-07-01 14:15:08 -0300

    feat(layout in example): better layout in example, showing the use of custons colors in pod

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -16,6 +16,28 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         myCodeInputView.delegate = self
+        setBackground()
+    }
+    
+    func setBackground() {
+        let gradientLayer = CAGradientLayer()
+        self.view.backgroundColor = .white
+        
+        gradientLayer.frame = self.view.bounds
+        
+        let color1 = #colorLiteral(red: 0.3294117647, green: 0.2117647059, blue: 0.3882352941, alpha: 1).cgColor
+        let color2 = #colorLiteral(red: 0.2823529412, green: 0.1921568627, blue: 0.3490196078, alpha: 1).cgColor
+        let color3 = #colorLiteral(red: 0.2705882353, green: 0.1921568627, blue: 0.3450980392, alpha: 1).cgColor
+        let color4 = #colorLiteral(red: 0.1411764706, green: 0.1529411765, blue: 0.2549019608, alpha: 1).cgColor
+        gradientLayer.colors = [color1, color2, color3, color4]
+        
+        gradientLayer.startPoint = CGPoint(x: 0.35, y: 0.25)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.6)
+        gradientLayer.zPosition = -1
+        
+        gradientLayer.locations = [0.0, 0.25, 0.75, 1.0]
+        
+        self.view.layer.addSublayer(gradientLayer)
     }
 }
 
